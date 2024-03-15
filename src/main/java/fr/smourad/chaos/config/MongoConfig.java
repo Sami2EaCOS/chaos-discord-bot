154178c05b9d28d5940b5e7764c9b63fd2b3a2ec
Sami MOURAD <mourads2001@gmail.com>
2024-03-15 20:08:33 +0100

    First commit

--- src/main/java/fr/smourad/chaos/config/MongoConfig.java
+++ src/main/java/fr/smourad/chaos/config/MongoConfig.java
@@ -5,7 +5,7 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.data.mongodb.repository.config.EnableReactiveMongoRepositories;
 
 @Configuration
-@EnableReactiveMongoRepositories(basePackages = {"me.smourad.chaos"})
+@EnableReactiveMongoRepositories(basePackages = {"fr.smourad.chaos"})
 public class MongoConfig { }
 
 
